71d1e39bdf3dd3208dd4cfff8a72962402a67407
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-03 19:02:56 +0000

    Adding chorus-while.js

---
+++ chorus-while.js
@@ -0,0 +1,17 @@
+var chorus = "Let's dance!";
+/*var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*")
+  }
+  console.log(chorus);
+  repeat += 1;
+}
+
+*/
+
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+
+console.log ("Until the sun comes up!");
