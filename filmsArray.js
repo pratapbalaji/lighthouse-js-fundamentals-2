a470aeb8ce9d7455e480f003d706f84716a143a8
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-03 00:19:38 +0000

    Adding filmsArray.js

---
+++ filmsArray.js
@@ -0,0 +1,16 @@
+var director = "Steven Spielberg";
+var films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+films[2] = "Jurrasic Park";
+films.push("A.I.");
+
+console.log(director + " is the director of " + films.length + " films");
+
+films.push("E.T.");
+
+console.log(director + " is the director of " + films.length + " films");
+
+
+//console.log(films);
+
+//console.log("My favourite " + director + " film is " + films[1]);
